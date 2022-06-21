5a6a0669d998988c8df19304f0b1be70fc013c6c
jhonthe77 <jhonthe7@gmail.com>
2022-06-21 04:21:22 -0500

    agrego comentarios al codigo

--- src/controller/controlador.js
+++ src/controller/controlador.js
@@ -1,25 +1,27 @@
 var tokens= require('jsonwebtoken');
 
 const controller = {};
-
+//----------------------------------------------------------------
 controller.registro = (req, res) => {
   res.render("registro");
 
 };
+//----------------------------------------------------------------
 
-
+//render de la vista registrado
 controller.registrado = (req, res) => {
     res.render("registrado");
-  
   };
+//----------------------------------------------------------------
 
-
+//render de la vista loguin-------------
 controller.loguin = (req, res) => {
   res.render("loguin");
 };
+//----------------------------------------------------------------
 
 
-
+//creo una constante para validar con expressions regular para validar los datos
 const expresiones = {
   nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
@@ -29,8 +31,8 @@ const expresiones = {
 
 
 controller.save = (req, res) => {
+  //realizo una validacion de datos para evitar error en los datos
   const { Nombre, Apellido, Numero_documento, Correo, Telefono } = req.body;
-
   if (
     expresiones.nombre.test(Nombre) &&
     expresiones.nombre.test(Apellido) &&
@@ -39,6 +41,7 @@ controller.save = (req, res) => {
     Numero_documento.length > 6 &&
     Numero_documento.length < 11
   ) {
+ //inserto un registro en la base de datos
     req.getConnection((err, conn) => {
       if (err) throw err;
       //metodo de registros seguro evita inyeccion sql
@@ -52,15 +55,18 @@ controller.save = (req, res) => {
     res.send("No se pudo terminar proceso ");
   }
 };
+//-------------------------------------------------------------
 
 
+//constante para exportacion de la function
 controller.cosulatar = (req, res) => {
-
+//genero un token para el autenticar el ususario
     function token(user) {
         return tokens.sign(user, process.env.SECRETO, { expiresIn:"10m" });
       };
+//--------------------------------------------------------------
 
-
+//verifico que el ususario exista en la base de datos
   const { password } = req.body;
   req.getConnection((err, conn) => {
     conn.query(
@@ -78,5 +84,6 @@ controller.cosulatar = (req, res) => {
   });
   
 };
+//----------------------------------------------------------------
 
 module.exports= controller;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// exporto mis dependency 
 const express = require('express');
 const paht = require('path');
 const morgan = require('morgan');
@@ -5,14 +6,19 @@ const mysql = require('mysql');
 const myconne = require('express-myconnection');
 const bodyParser = require('body-parser');
 require('dotenv').config()
-
-
-
+// ----------------------------------------------------------------
 
 const app = express();
+
+
+//configuro las las vistas y sus rutas
 app.set('view engine', 'ejs');
 app.set('views', paht.join(__dirname,'views'));
 app.use(morgan('dev'));
+//----------------------------------------------------------------
+
+
+//configuracion de mi base de datos
 app.use(myconne(mysql, {
     host: "localhost",
     user: "jhon",
@@ -21,21 +27,24 @@ app.use(myconne(mysql, {
     database: "loguin",
 
 }, 'single'))
-
-
+//----------------------------------------------------------------
+// export mis rutas para que el servidor las procese
 const rutas = require('./routes/rutas');
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', rutas);
+//---------------------------------------------------------------
+
+//configuro mi servidor para que pueda leer los diferen datos que le enviare 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+//------------------------------------------------------------
 
 
 
-
-
+//configuro mi servidor en el puerto 9000
 app.listen(9000, (err) => {
     if (err) throw err
     console.log('server on port 9000');
 });
-
+//------------------------------------------------------------
 
 
